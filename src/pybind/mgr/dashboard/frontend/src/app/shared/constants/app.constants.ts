f6cf755744d50eef1ca31bc99f3edc2ce0b14ca9
Sarthak0702 <sarthak.dev.0702@gmail.com>
2022-06-03 04:28:31 +0530

    mgr/dashboard: rbd force resync from fornt-end
    Resolves: rhbz#1891012
    
    Signed-off-by: Sarthak0702 <sarthak.dev.0702@gmail.com>
    (cherry picked from commit 2b005395818700185e2c0f7713fae0c321471b2d)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/constants/app.constants.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/constants/app.constants.ts
@@ -137,6 +137,7 @@ export class ActionLabelsI18n {
   STOP: string;
   REDEPLOY: string;
   RESTART: string;
+  RESYNC: string;
 
   constructor() {
     /* Create a new item */
@@ -193,7 +194,7 @@ export class ActionLabelsI18n {
 
     this.START_DRAIN = $localize`Start Drain`;
     this.STOP_DRAIN = $localize`Stop Drain`;
-
+    this.RESYNC = $localize`Resync`;
     /* Prometheus wording */
     this.RECREATE = $localize`Recreate`;
     this.EXPIRE = $localize`Expire`;
