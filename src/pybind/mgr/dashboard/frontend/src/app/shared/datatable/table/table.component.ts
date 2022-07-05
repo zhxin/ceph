a2cd0e08014335f9775d9f8779b35c640d5362de
Pere Diaz Bou <pdiazbou@redhat.com>
2022-07-05 11:55:51 +0200

    mgr/dashboard: change sort char to '+' and page icons
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>
    (cherry picked from commit c01ce41266aeec229d60c302b4da8ed6fac6f2d0)
    (cherry picked from commit 25b8ce92f93407bf29cb0dd4015c4a35cb37d4dd)
    Resolves: rhbz#2125432

--- src/pybind/mgr/dashboard/frontend/src/app/shared/datatable/table/table.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/datatable/table/table.component.ts
@@ -656,7 +656,7 @@ export class TableComponent implements AfterContentChecked, OnInit, OnChanges, O
       context.search = this.userConfig.search;
       if (this.userConfig.sorts?.length) {
         const sort = this.userConfig.sorts[0];
-        context.sort = `${sort.dir === 'desc' ? '<' : '>'}${sort.prop}`;
+        context.sort = `${sort.dir === 'desc' ? '-' : '+'}${sort.prop}`;
       }
       this.fetchData.emit(context);
       this.updating = true;
@@ -797,7 +797,8 @@ export class TableComponent implements AfterContentChecked, OnInit, OnChanges, O
   changeSorting({ sorts }: any) {
     this.userConfig.sorts = sorts;
     if (this.serverSide) {
-      this.reloadData();
+			this.userConfig.offset = 0;
+			this.reloadData();
     }
   }
 
